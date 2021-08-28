d674be4e6c9e1967fb8cf4308690821a6ba4d4c9
PScoriae <pierreccesario@gmail.com>
2021-08-29 00:17:39 +0800

    modularized

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Require the necessary discord.js classes
 const { Client, Collection, Intents } = require('discord.js');
-const fs = require('fs');
 const { token } = require('./config.json');
+const fs = require('fs');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -12,6 +12,17 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.data.name, command);
+}
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
 }
 
 // When the client is ready, run this code (only once)
