647732014486659eba4b6651bb0c2ff29d678828
PScoriae <pierreccesario@gmail.com>
2021-08-30 22:31:42 +0800

    refactor and bug fix

--- commands/pic.js
+++ commands/pic.js
@@ -2,6 +2,65 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+const site = "https://kpop.asiachan.com";
+
+const getRestrictedRange = (noOfPages) => {
+  if (noOfPages > 100) {
+    return 100;
+  }
+  return noOfPages;
+};
+
+const getRndInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + 1) + min;
+};
+
+const getRandomPage = async (name) => {
+  const searchString = `${site}/${name}?s=id`;
+  const values = [];
+  try {
+    const { data } = await axios({
+      method: "GET",
+      url: searchString,
+    });
+
+    const $ = cheerio.load(data);
+    const pagination = $(".pagination").text().trim();
+    const tmp = pagination.split(" ")[3];
+    const re = /\d+/;
+    const noOfPages = Number(tmp.match(re)[0]);
+    const randomPage = String(getRndInt(1, getRestrictedRange(noOfPages)));
+    values.push(searchString);
+    values.push(randomPage);
+    return values;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getImageLink = async (searchString, randomPage) => {
+  searchString = `${searchString}&p=${randomPage}`;
+  try {
+    const { data } = await axios({
+      method: "GET",
+      url: searchString,
+    });
+
+    const $ = cheerio.load(data);
+    const links = [];
+    $("#thumbs2 > li > p").each((i, pic) => {
+      const tmp = $(pic).children("a").attr("href")
+      if (tmp.includes('full')) {
+        links.push(tmp);
+      }
+    });
+    const randomIdx = getRndInt(0, links.length - 1);
+    return links[randomIdx];
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("pic")
@@ -13,57 +72,10 @@ module.exports = {
         .setRequired(true)
     ),
   async execute(interaction) {
-    const getRestrictedRange = (noOfPages) => {
-      if (noOfPages > 100) {
-        return 100;
-      }
-      return noOfPages;
-    };
-
-    const getRndInt = (min, max) => {
-      return Math.floor(Math.random() * (max - min) + 1) + min;
-    };
-
-    const getImage = async (arg) => {
-      const site = "https://kpop.asiachan.com";
-      const searchString = `${site}/${arg}?s=id`;
-      try {
-        const { data } = await axios({
-          method: "GET",
-          url: searchString,
-        });
-
-        const $ = cheerio.load(data);
-        const pagination = $(".pagination").text().trim();
-        const tmp = pagination.split(" ")[3];
-        const re = /\d+/;
-        const noOfPages = Number(tmp.match(re)[0]);
-        const randomPage = String(getRndInt(1, getRestrictedRange(noOfPages)));
-
-        const searchString2 = `${searchString}&p=${randomPage}`;
-        try {
-          const { data } = await axios({
-            method: "GET",
-            url: searchString2,
-          });
-
-          const $ = cheerio.load(data);
-          const links = [];
-          $("#thumbs2 > li > p").each((i, pic) => {
-            links.push($(pic).children("a").attr("href"));
-          });
-          const randomIdx = getRndInt(0, links.length - 1);
-          return links[randomIdx];
-        } catch (err) {
-          console.log(err);
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    const string = interaction.options.getString("name");
     await interaction.deferReply();
-    const link = await getImage(string);
+    const name = interaction.options.getString("name");
+    const values = await getRandomPage(name);
+    const link = await getImageLink(values[0], values[1]);
     await interaction.editReply(link);
   },
 };
