81646aa76e73202a9025ac841e3d1b614d02e55b
PScoriae <pierreccesario@gmail.com>
2021-08-31 00:04:54 +0800

    added global deploy commands script

--- deploy-commands-global.js
+++ deploy-commands-global.js
@@ -1,6 +1,6 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, token } = require('./config.json');
 const fs = require('fs');
 
 const commands = [];
@@ -16,7 +16,7 @@ const rest = new REST({ version: '9' }).setToken(token);
 (async () => {
 	try {
 		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 
