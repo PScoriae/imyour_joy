cee64535e0e3d2890c00289a44313ead34114e94
PScoriae <pierreccesario@gmail.com>
2021-08-31 14:37:43 +0800

    modularised for easy configuration

--- docker.sh
+++ docker.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
 # automates docker image creation and push to local registry.
 
-docker build -t imyourjoy .
-docker tag imyourjoy 192.168.0.154:5000/imyourjoy
-docker push 192.168.0.154:5000/imyourjoy
+imageName=imyourjoy
+serverIp=192.168.0.154
+serverPort=5000
+
+docker build -t $imageName .
+docker tag imyourjoy $serverIp:$serverPort/$imageName
+docker push $serverIp:$serverPort/$imageName
