6b68c9900dc7e4b28e17bc73e4b8507dcc6c0849
PScoriae <pierreccesario@gmail.com>
2021-08-31 23:17:09 +0800

    prettified

--- deploy_commands/deploy-commands-global.js
+++ deploy_commands/deploy-commands-global.js
@@ -1,27 +1,26 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { clientId, token } = require('../config.json');
-const fs = require('fs');
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const { clientId, token } = require("../config.json");
+const fs = require("fs");
 
 const commands = [];
-const commandFiles = fs.readdirSync('../commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("../commands")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const command = require(`../commands/${file}`);
-	commands.push(command.data.toJSON());
+  const command = require(`../commands/${file}`);
+  commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: "9" }).setToken(token);
 
 (async () => {
-	try {
-		await rest.put(
-			Routes.applicationCommands(clientId),
-			{ body: commands },
-		);
+  try {
+    await rest.put(Routes.applicationCommands(clientId), { body: commands });
 
-		console.log('Successfully registered application commands globally.');
-	} catch (error) {
-		console.error(error);
-	}
+    console.log("Successfully registered application commands globally.");
+  } catch (error) {
+    console.error(error);
+  }
 })();
