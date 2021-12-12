021c988abc15486abb0ca20260dd00ef08a74bca
PScoriae <pierreccesario@gmail.com>
2021-12-12 12:57:02 +0800

    fixed daily song feature, refactored functions

--- functions.js
+++ functions.js
@@ -1,5 +1,86 @@
+const { ytApiKey, spotify } = require("./config.json");
+const SpotifyWebApi = require("spotify-web-api-node");
+const YouTube = require("youtube-node");
+const fs = require("fs");
+
 function getRandInt(length) {
   return Math.floor(Math.random() * length);
 }
 
-module.exports = { getRandInt };
+function getAllDirFiles(dirPath, arrayOfFiles) {
+  const files = fs.readdirSync(dirPath);
+
+  arrayOfFiles = arrayOfFiles || [];
+
+  files.forEach(function (file) {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllDirFiles(dirPath + "/" + file, arrayOfFiles);
+    } else {
+      arrayOfFiles.push(file);
+    }
+  });
+
+  return arrayOfFiles;
+}
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: spotify.clientId,
+  clientSecret: spotify.clientSecret,
+  redirectUri: "http://localhost:8000/callback",
+});
+
+spotifyApi.setRefreshToken(spotify.refreshToken);
+
+const youTube = new YouTube();
+youTube.setKey(ytApiKey);
+
+const ytPrefix = "https://youtu.be/";
+const kpopPlaylistId = spotify.playlistId;
+
+function refreshSpotifyAccessToken() {
+  spotifyApi.refreshAccessToken().then(function (data) {
+    spotifyApi.setAccessToken(data.body.access_token);
+    console.log("refreshed spotify access token!");
+  });
+}
+
+async function sendRandSong(textChannel) {
+  refreshSpotifyAccessToken();
+  await new Promise((r) => setTimeout(r, 2000));
+  spotifyApi.getPlaylistTracks(kpopPlaylistId).then(
+    function (data) {
+      const randomNo = getRandInt(data.body.items.length);
+      const searchTerm =
+        data.body.items[randomNo].track.name +
+        " " +
+        data.body.items[randomNo].track.artists[0].name;
+      youTube.search(searchTerm, 1, function (error, result) {
+        if (error) {
+          console.log(error);
+        } else {
+          const ytLink = ytPrefix + result.items[0].id.videoId;
+          textChannel.send(ytLink);
+        }
+      });
+    },
+    async function (err) {
+      console.log(err);
+      // refreshSpotifyAccessToken();
+      // await new Promise((r) => setTimeout(r, 2000));
+      // sendRandSong(textChannel);
+    }
+  );
+}
+
+function changeGuildIcon(imageDirFiles, myGuild) {
+  const randomNo = getRandInt(imageDirFiles.length);
+  const chosenImg = imageDirFiles[randomNo];
+  imageDirFiles.splice(randomNo, 1);
+  myGuild.setIcon("./images/" + chosenImg);
+
+  if (imageDirFiles.length < 1) {
+    imageDirFiles = getAllDirFiles("./images/");
+  }
+}
+
+module.exports = { getAllDirFiles, sendRandSong, changeGuildIcon };
--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 // Require the necessary discord.js classes
 const { Client, Collection, Intents } = require("discord.js");
+const { discordToken, guildId, targetChannel } = require("./config.json");
 const {
-  discordToken,
-  guildId,
-  testingChannel,
-  ytApiKey,
-  spotify,
-} = require("./config.json");
-const { getRandInt } = require("./functions.js");
+  getAllDirFiles,
+  sendRandSong,
+  changeGuildIcon,
+} = require("./functions.js");
 const fs = require("fs");
 const cron = require("node-cron");
-const YouTube = require("youtube-node");
-const SpotifyWebApi = require("spotify-web-api-node");
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -41,11 +37,8 @@ for (const file of eventFiles) {
 
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
-
   const command = client.commands.get(interaction.commandName);
-
   if (!command) return;
-
   try {
     await command.execute(interaction);
   } catch (error) {
@@ -57,96 +50,17 @@ client.on("interactionCreate", async (interaction) => {
   }
 });
 
-const getAllDirFiles = function (dirPath, arrayOfFiles) {
-  const files = fs.readdirSync(dirPath);
-
-  arrayOfFiles = arrayOfFiles || [];
-
-  files.forEach(function (file) {
-    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-      arrayOfFiles = getAllDirFiles(dirPath + "/" + file, arrayOfFiles);
-    } else {
-      arrayOfFiles.push(file);
-    }
-  });
-
-  return arrayOfFiles;
-};
-
-const spotifyApi = new SpotifyWebApi({
-  clientId: spotify.clientId,
-  clientSecret: spotify.clientSecret,
-  redirectUri: "http://localhost:8000/callback",
-});
-
-const youTube = new YouTube();
-youTube.setKey(ytApiKey);
-
-const ytPrefix = "https://youtu.be/";
-const kpopPlaylistId = "5zvS8rhYah42PJfMkyNtt0";
-
-spotifyApi.setAccessToken(spotify.accessToken);
-spotifyApi.setRefreshToken(spotify.refreshToken);
-
-let imageDirFiles = getAllDirFiles("./images/");
-
 client.on("ready", async () => {
+  let imageDirFiles = getAllDirFiles("./images/");
   const myGuild = client.guilds.cache.get(guildId);
-  const joybotChannel = client.channels.cache.get(testingChannel);
+  const musicChannel = client.channels.cache.get(targetChannel);
 
   cron.schedule("0 * * * *", async () => {
-    const randomNo = getRandInt(imageDirFiles.length);
-    const chosenImg = imageDirFiles[randomNo];
-    imageDirFiles.splice(randomNo, 1);
-    myGuild.setIcon("./images/" + chosenImg);
-
-    if (imageDirFiles.length < 1) {
-      imageDirFiles = getAllDirFiles("./images/");
-    }
+    changeGuildIcon(imageDirFiles, myGuild);
   });
 
-  cron.schedule("30 * * * *", async () => {
-    spotifyApi.getPlaylistTracks(kpopPlaylistId).then(
-      function (data) {
-        const randomNo = getRandInt(data.body.items.length);
-        const searchTerm =
-          data.body.items[randomNo].track.name +
-          " " +
-          data.body.items[randomNo].track.artists[0].name;
-        youTube.search(searchTerm, 1, function (error, result) {
-          if (error) {
-            console.log(error);
-          } else {
-            const ytLink = ytPrefix + result.items[0].id.videoId;
-            joybotChannel.send(ytLink);
-          }
-        });
-      },
-      function (err) {
-        console.log("error :(", err);
-      }
-    );
-  });
-
-  // refresh access token every 10 minutes
-  cron.schedule("/*10 * * * *", async () => {
-    // When our access token will expire
-    let tokenExpirationEpoch;
-
-    spotifyApi.refreshAccessToken().then(
-      function (data) {
-        tokenExpirationEpoch =
-          new Date().getTime() / 1000 + data.body["expires_in"];
-        console.log(
-          "Refreshed token. It now expires in " +
-            Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) +
-            " seconds!"
-        );
-      },
-      function (err) {
-        console.log("Could not refresh the token!", err.message);
-      }
-    );
+  cron.schedule("0 15 * * *", async () => {
+    await sendRandSong(musicChannel);
   });
 });
 
