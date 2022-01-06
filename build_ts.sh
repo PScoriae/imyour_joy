98d460b478d9c9711ec673b04e5c3060a25c0a75
PScoriae <pierreccesario@gmail.com>
2022-01-06 20:29:17 +0800

    add comments

--- build_ts.sh
+++ build_ts.sh
@@ -1,5 +1,7 @@
 # automated script to clean dist folder then transpile
 
+# clear old build files
 sudo rm -rf ./dist/
 
+# node package execute (npx) removes need for global tsc install
 npx tsc -p .
