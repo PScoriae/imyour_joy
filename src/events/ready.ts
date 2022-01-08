cb2faeb2eaab553ccb510010bebbf9e31e82a662
PScoriae <pierreccesario@gmail.com>
2022-01-08 21:11:11 +0800

    style: use prettier autoformatting

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,12 @@
 import { Client } from "discord.js";
+const { getCurrentTime } = require("../functions");
 
 module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client: Client) {
-		console.log(`Ready! Logged in as ${client.user?.tag}`);
-		client.user?.setActivity('with Pie');
-	},
+  name: "ready",
+  once: true,
+  execute(client: Client) {
+    console.log(getCurrentTime());
+    console.log(`Ready! Logged in as ${client.user?.tag}`);
+    client.user?.setActivity("with Pie");
+  },
 };
